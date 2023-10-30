7f835d0dd679b4c81ee2986ce32d61ad4d13af8e
Leonel Ramella <leonelramella30@gmail.com>
2023-10-30 00:32:58 -0300

    Resuelto

---
+++ js/ejercicio1.js
@@ -0,0 +1,10 @@
+let numero1 = 10;
+let numero2 = 2;
+const sumaNumero = numero1+numero2;
+console.log('Resultado de la Suma es ' + sumaNumero);
+const RestaNumero = numero1-numero2;
+console.log('Resultado de la Resta es ' + RestaNumero);
+const MultiplicarNumero = numero1*numero2;
+console.log('Resultado de la Multiplicaión es ' + MultiplicarNumero);
+const DividirNumero = numero1/numero2;
+console.log('Resultado de la División es ' + DividirNumero);
